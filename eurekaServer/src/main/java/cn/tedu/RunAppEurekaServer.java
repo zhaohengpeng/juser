e96a539bdde25f4f6a56ce46649d35f3d897d6eb
zhaohengpeng <zhaohengpengj@163.com>
2018-11-09 14:31:37 +0800

    text

---
+++ eurekaServer/src/main/java/cn/tedu/RunAppEurekaServer.java
@@ -0,0 +1,14 @@
+package cn.tedu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+@SpringBootApplication
+@EnableEurekaServer
+public class RunAppEurekaServer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RunAppEurekaServer.class, args);
+	}
+
+}
